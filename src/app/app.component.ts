998f91badf5ce30a50468a2be402f46b3cdb7e2a
Bhushan Pawar <bhushanpawar@mail.fresnostate.edu>
2019-03-03 21:07:44 -0800

    initial google anf faceboook auth set up done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth } from "firebase/app";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'FirebaseAuthenticationApp';
+  title = "FirebaseAuthenticationApp";
+  constructor(public afAuth: AngularFireAuth) {}
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  } 
+  fbLogin(){
+    this.afAuth.auth.signInWithPopup(new auth.FacebookAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
+
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+
+import { AuthService } from "./services/auth.service";
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDJxzSvzWHJCywVbHWnxl20JcSa2iiM_qw",
+  authDomain: "angularfirebaseauthproject.firebaseapp.com",
+  databaseURL: "https://angularfirebaseauthproject.firebaseio.com",
+  projectId: "angularfirebaseauthproject",
+  storageBucket: "",
+  messagingSenderId: "142997162845"
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, UserLoginComponent, UserProfileComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
